d7ea8f7eeb8dff29190e7c02ebacbc0c626c1e32
Juan Curti <juancurti@Juans-MacBook-Pro.local>
2021-06-23 19:29:06 +0200

    Refactor

--- Game.sol
+++ Game.sol
@@ -2,41 +2,56 @@
 
 pragma solidity 0.6.11;
 pragma experimental ABIEncoderV2;
-import './UserProfile.sol';
 import './IStdReference.sol';
 
-contract Game {
+contract Game{
     IStdReference _ref;
-    uint256 private _pool;
-    UserProfile private _playera;
-    UserProfile private _playerb;
+    address _owner;
     
-    uint256 private _creationDate;
-    uint256 private _initialValue;
+    uint256 private _totalUsers;
+    mapping(uint256 => address) private _user;
+    mapping(address => uint256) private _totalRewards;
     
-    constructor(IStdReference ref) public {
-        _ref = ref;
-        _creationDate = getBTCLastDate();
-        _initialValue = getBTCPrice();
+    constructor() public {
+        _ref = IStdReference(0xDA7a001b254CD22e46d3eAB04d937489c93174C3);
+        _owner = msg.sender;
     }
     
-    function getCreationDate() public view returns (uint256) {
-        return _creationDate;
+    //Rewards
+    function totalRewards() external view returns (uint256) {
+        if(_totalUsers == 0) {
+            return 0;
+        }else{
+            uint256 _total = 0;
+            
+            for(uint256 i=1; i < _totalUsers;i++) {
+                _total = _total + _totalRewards[_user[i]];
+            }
+            
+            return _total;
+        }
     }
     
-    function getInitialValue() public view returns (uint256) {
-        return _initialValue;
+    function rewardsByPlayer(address player) external view returns (uint256) {
+        if(_totalUsers == 0) {
+            return 0;
+        }else{
+            uint256 _userRewards;
+            
+            for(uint256 i=1; i < _totalUsers;i++) {
+                if(_user[i] == player) {
+                    _userRewards = _totalRewards[_user[i]];
+                }
+            }
+            
+            return _userRewards;
+        }
     }
     
     //Bitcoin price fetching
-    function getBTCPrice() private view returns (uint256){
+    function getLastBTCPrice() external view returns (uint256){
         IStdReference.ReferenceData memory data = _ref.getReferenceData("BTC","USD");
         return data.rate;
-    }
-    
-    function getBTCLastDate() private view returns (uint256){
-        IStdReference.ReferenceData memory data = _ref.getReferenceData("BTC","USD");
-        return data.lastUpdatedQuote;
     }
 
 }
